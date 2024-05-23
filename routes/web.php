4774dfe3b8ca87398c61081447918676aba81b59
Dellon Valentino Ardi <159220578+LenDlenDlen@users.noreply.github.com>
2024-05-24 02:50:59 +0700

    HomePage
    front end homepage

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/logout', [LoginController::class, 'logout']);
 
 
+
 Route::middleware('guest')->group(function(){
     Route::get('/', [LoginController::class, 'showLandingPage'])->name('login');
 
