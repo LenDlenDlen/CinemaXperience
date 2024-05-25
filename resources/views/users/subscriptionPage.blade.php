33fbe985dc6dff9f055650684c97db3889095c0c
selika <putriselika@gmail.com>
2024-05-25 21:10:51 +0700

    add subscription page file

---
+++ resources/views/users/subscriptionPage.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title', 'Subscription Page')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/subscriptionPage.css') }}">
+@endsection
+    
+</body>
+</html>
+
+@section('content')
+<!-- Isi semua di sini -->
+<div>
+    <h1>
+        ini subscription page yah
+    </h1>
+</div>
+
+@endsection
