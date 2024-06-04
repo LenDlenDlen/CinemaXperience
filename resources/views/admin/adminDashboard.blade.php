d33f168f1f52e81574230db74f3636c488ba3eae
Dellon Valentino Ardi <159220578+LenDlenDlen@users.noreply.github.com>
2024-06-04 10:00:40 +0700

    tiny updates

--- resources/views/admin/adminDashboard.blade.php
+++ resources/views/admin/adminDashboard.blade.php
@@ -3,9 +3,12 @@
 @section('title', 'Admin Dashboard')
 
 @section('css')
-
+<link rel="stylesheet" href="{{ asset('css/AdminPage.css') }}">
 @endsection
 
 @section('content')
 <h1 style="color: white">INI ADMIN DASHBOARD</h1>
+<div>
+    
+</div>
 @endsection
